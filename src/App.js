618a3ff676fe9674189cf74c947b5362d6ddd89c
Saurabh Suman <saurabh22suman@gmail.com>
2021-02-21 20:51:16 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <button className="btn btn-primary"  >Subscribe</button>
+      <button className="btn btn-secondary"  >Subscribe</button>
     </div>
   );
 }
